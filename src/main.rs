41c9b01c4f2fe11daa2f71671ad7623fe4e05633
TakuyaUrushibara <ut1995j15@gmail.com>
2021-08-10 15:40:12 +0900

    clapクレートの導入とサンプルの実施

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Takuya",
+    about = "Super awesome sample RPN caculator"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
